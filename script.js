593005e12633ec652d1da2ae326c8b949b64fcb9
spielchen <anna.bukavtsova@gmail.com>
2019-03-04 19:49:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 
 
 //console.log((week[0])+"\n"+(week[1])+"\n"+(week[2])+"\n"+(week[3])+"\n"+(week[4])+"\n"+(week[5])+"\n"+(week[6]));
 
-for(var i=0; i < week.length-2; i++){
-	console.log((week[i])+"\n");
+for(var i=0; i < week.length; i++){
+	//console.log((week[i])+"\n");
+		//let newDiv = document.createElement("div");
+		var newDiv = document.write(week[i] + "<br \/>");
+		
 }
 
-console.log('%c%s', 'font: italic bold 1.3em/1 Arial', week[5]);
-console.log('%c%s', 'font: bold 1.3em/1 Arial', week[6]);
+week[5].style.font = ('italic');
+
+
+//console.log('%c%s', 'font: italic bold 1.3em/1 Arial', week[5]);
+//console.log('%c%s', 'font: bold 1.3em/1 Arial', week[6]);
+
+
 
 var numbers = ["3343", "43434", "5646", "1231", "675354", "6777", "7777", "3333"];
-console.log(numbers);
+
 for (i=0; i<numbers.length; i++){
 	var firstNum = numbers[i].charAt(0);
 	if( firstNum=="3"){
